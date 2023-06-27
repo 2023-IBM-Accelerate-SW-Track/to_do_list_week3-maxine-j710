46521bd3d27853bf83ff3ee2e11305263625488e
Genevieve Jean-Pierre <genevieve.jeanpierre@duke.edu>
2023-06-26 20:03:30 -0500

    the date comparison

--- src/component/todos.js
+++ src/component/todos.js
@@ -12,9 +12,18 @@ import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material
 const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
+      let color = "white";
+      let isExpired = false;
+      if ( new Date() > new Date(todo.due)){
+        console.log(todo.date, todo.due)
+        color = "red";
+
+
+      }
+
       return (
         <Grid key={todo.id}>
-          <Card style={{marginTop:10}}>
+          <Card style={{marginTop:10, background: {color}}} data-testid = {todo.state.content} >
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
             current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}.
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,6 +30,13 @@ class Home extends Component {
   addTodo = (todo) => {
     const exists = this.state.todos.find(t => t.content === todo.content);
     if (exists){ return }
+
+    if (this.state.todos.duedate === null || "Invalid Date" )
+    { return }
+
+
+
+
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
