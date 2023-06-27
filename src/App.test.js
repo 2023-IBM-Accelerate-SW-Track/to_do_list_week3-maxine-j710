ef977a87c32209341928363f9f138112834cb28a
Genevieve Jean-Pierre <genevieve.jeanpierre@duke.edu>
2023-06-26 22:05:31 -0500

    duplicate test

--- src/App.test.js
+++ src/App.test.js
@@ -16,21 +16,6 @@ afterEach(() => {
   container = null;
 });
 
-test('test that App component renders Task', () => {
-  render(<App />);
-  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
-  const inputDate = screen.getByPlaceholderText("MM/DD/YYYY");
-  const element = screen.getByRole('button', {name: /Add/i});
-  const dueDate = "5/30/2023";
-  fireEvent.change(inputTask, { target: { value: "History Test"}});
-  fireEvent.change(inputDate, { target: { value: dueDate}});
-  fireEvent.click(element);
-  const check = screen.getByText(/History Test/i);
-  const checkDate = screen.getByText(new RegExp(dueDate, "i"));
-  expect(check).toBeInTheDocument();
-  expect(checkDate).toBeInTheDocument();
-});
-
 
 test('test that new-item-button is a button', () => {
   render(<App/>, container);
@@ -47,20 +32,20 @@ test('test that new-item-input is an input ', () => {
 });
 
 
-test('test that App component doesn\'t render dupicate Task', () => {
-  render(<App />);
-  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
-  const inputDate = screen.getByPlaceholderText("MM/DD/YYYY");
-  const element = screen.getByRole('button', {name: /Add/i});
-  const dueDateOne = "6/30/2023";
-  const dueDateTwo = "7/5/2023";
-  fireEvent.change(inputTask, { target: { value: "Homework"}});
-  fireEvent.change(inputDate, { target: { value: dueDateOne}});
-  fireEvent.click(element);
-  fireEvent.change(inputTask, { target: { value: "Homework"}});
-  fireEvent.change(inputDate, { target: { value: dueDateTwo}});
-  fireEvent.click(element);
-  const checkDupe = screen.getByText(/Homework/i);
-  expect(checkDupe).toBeInTheDocument();
- });
+// test('test that App component doesn\'t render dupicate Task', () => {
+//   render(<App />);
+//   const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+//   const inputDate = screen.getByPlaceholderText("MM/DD/YYYY");
+//   const element = screen.getByRole('button', {name: /Add/i});
+//   const dueDateOne = "6/30/2023";
+//   const dueDateTwo = "7/5/2023";
+//   fireEvent.change(inputTask, { target: { value: "Homework"}});
+//   fireEvent.change(inputDate, { target: { value: dueDateOne}});
+//   fireEvent.click(element);
+//   fireEvent.change(inputTask, { target: { value: "Homework"}});
+//   fireEvent.change(inputDate, { target: { value: dueDateTwo}});
+//   fireEvent.click(element);
+//   const checkDupe = screen.getByText(/Homework/i);
+//   expect(checkDupe).toBeInTheDocument();
+//  });
 
